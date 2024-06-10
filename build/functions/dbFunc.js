f20e9edf1335dc1001ec25608bfb770ab2b2b82a
thebleedingedge <thebleedingedge@yahoo.com>
2024-06-11 01:30:12 +0530

    update

--- build/functions/dbFunc.js
+++ build/functions/dbFunc.js
@@ -115,16 +115,18 @@ export async function search_document(searchTerms, page) {
         console.log(formattedSearchString); // Output: "\"thrones\" \"s02\" \"psa\" \"720p\""
         // Count filtered documents
         const totalsize = await db.DocumentCollection.countDocuments({
-            $text: { $search: formattedSearchString }
+            $text: { $search: formattedSearchString },
         });
         // Fetch filtered documents for the specified page
         const filteredDocs = await db.DocumentCollection.find({
-            $text: { $search: formattedSearchString }
+            $text: { $search: formattedSearchString },
         })
             .skip(skip)
             .limit(10)
             .toArray();
-        console.log(filteredDocs);
+        if (filteredDocs) {
+            console.log(filteredDocs);
+        }
         // Return an object containing both filtered documents and total size
         return { filteredDocs, totalsize };
     }
--- src/functions/dbFunc.ts
+++ src/functions/dbFunc.ts
@@ -121,15 +121,19 @@ export async function search_document(
 
     // Count filtered documents
     const totalsize = await db.DocumentCollection.countDocuments({
-      $text: { $search: formattedSearchString } });
+      $text: { $search: formattedSearchString },
+    });
     // Fetch filtered documents for the specified page
     const filteredDocs = await db.DocumentCollection.find({
-      $text: { $search: formattedSearchString } })
+      $text: { $search: formattedSearchString },
+    })
       .skip(skip)
       .limit(10)
       .toArray();
 
-    console.log(filteredDocs);
+    if (filteredDocs) {
+      console.log(filteredDocs);
+    }
 
     // Return an object containing both filtered documents and total size
     return { filteredDocs, totalsize };
