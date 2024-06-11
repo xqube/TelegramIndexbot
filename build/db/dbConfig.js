b750eaa78c6af66364384d5497169d7336c5c5b5
thebleedingedge <thebleedingedge@yahoo.com>
2024-06-11 22:21:35 +0530

    update commenting backup command

--- build/db/dbConfig.js
+++ build/db/dbConfig.js
@@ -11,16 +11,16 @@ export async function mongoconnect() {
         const AudioCollection = database.collection('audios');
         const UserCollection = database.collection('users');
         //Indexing is DB admins only command don't use it with applications, just written here for reference
-        await DocumentCollection.createIndex({ file_unique_id: 1 }, { unique: true });
+        // await DocumentCollection.createIndex({ file_unique_id: 1 }, { unique: true });
         // await DocumentCollection.createIndex({ file_name: 1 });
         // await DocumentCollection.createIndex({ is_banned: 1 });
-        await VideoCollection.createIndex({ file_unique_id: 1 }, { unique: true });
+        // await VideoCollection.createIndex({ file_unique_id: 1 }, { unique: true });
         // await VideoCollection.createIndex({ file_name: 1 });
         // await VideoCollection.createIndex({ is_banned: 1 });
-        await AudioCollection.createIndex({ file_unique_id: 1 }, { unique: true });
+        // await AudioCollection.createIndex({ file_unique_id: 1 }, { unique: true });
         // await AudioCollection.createIndex({ file_name: 1 });
         // await AudioCollection.createIndex({ is_banned: 1 });
-        await UserCollection.createIndex({ user_id: 1 }, { unique: true });
+        // await UserCollection.createIndex({ user_id: 1 }, { unique: true });
         // await UserCollection.createIndex({ is_banned: 1 });
         return { DocumentCollection, VideoCollection, AudioCollection, UserCollection, database };
     }
