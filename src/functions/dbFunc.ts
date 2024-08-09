5828f00d9f7fa3ffb4cca7bacffa0dfd1ec845cf
thebleedingedge <thebleedingedge@yahoo.com>
2024-08-09 17:29:20 +0530

    commit

--- src/functions/dbFunc.ts
+++ src/functions/dbFunc.ts
@@ -292,12 +292,13 @@ export async function get_db_data(): Promise<any | null> {
       scale: 1024,
       freeStorage: 1,
     });
+    const totalsize = await db.documents.aggregate([{ $group: { _id: null, totalFileSize: { $sum: "$file_size" } } }])
     const doc_result = await db.DocumentCollection.countDocuments();
     const vid_result = await db.VideoCollection.countDocuments();
     const aud_result = await db.AudioCollection.countDocuments();
     const user_data = await db.UserCollection.countDocuments();
 
-    return { dbdata, doc_result, vid_result, aud_result, user_data };
+    return { dbdata, doc_result, vid_result, aud_result, user_data, totalsize };
   } catch (error: any) {
     console.log(error.message);
     return null; // Return null in case of error
